cc972311f0bbb6521100d0c6103c26ec22b88d0b
Junphill Hwang <444.gkk@gmail.com>
2019-09-04 17:57:18 +0900

    search algorithm

--- main_app/views.py
+++ main_app/views.py
@@ -14,85 +14,61 @@ class IndexView(TemplateView):
 
 
 class SearchView(View):
+    def create_rank_result(self, result, summoner):
+        for rank_result_champion in result['rank_result'].keys():
+            champion = get_object_or_404(Champion, champion_key=rank_result_champion)
+
+            win = result['rank_result'][rank_result_champion]['win']
+            lose = result['rank_result'][rank_result_champion]['lose']
+            games = result['rank_result'][rank_result_champion]['games']
+            kills = result['rank_result'][rank_result_champion]['kills']
+            deaths = result['rank_result'][rank_result_champion]['deaths']
+            assists = result['rank_result'][rank_result_champion]['assists']
+
+            try:
+                average = round((result['rank_result'][rank_result_champion]['kills'] +
+                                 result['rank_result'][rank_result_champion]['assists']) /
+                                result['rank_result'][rank_result_champion]['deaths'], 2)
+            except ZeroDivisionError:
+                average = None
+
+            winning_rate = round(
+                result['rank_result'][rank_result_champion]['win'] / result['rank_result'][rank_result_champion][
+                    'games'] * 100)
+
+            RankGameResult.objects.create(champion=champion, summoner=summoner,
+                                          win=win,
+                                          lose=lose,
+                                          games=games,
+                                          kills=kills,
+                                          deaths=deaths,
+                                          assists=assists,
+                                          average=average,
+                                          winning_rate=winning_rate
+                                          )
+
     def get(self, request, *args, **kwargs):
         context = {}
         summoner_name = self.request.GET.get('summoner_name')
         print(summoner_name)
-        result = get_rank_result(summoner_name)
-        print(result)
+        # result = get_rank_result(summoner_name)
 
-        summoner, created = Summoner.objects.get_or_create(summoner_name=result['summoner']['name'],
-                                       account_id=result['summoner']['accountId'])
+        summoner, created = Summoner.objects.get_or_create(summoner_name=summoner_name)
+        # summoner, created = Summoner.objects.get_or_create(summoner_name=result['summoner']['name'],
+                                       # account_id=result['summoner']['accountId'])
 
         if created:
             print('created')
-            for rank_result_champion in result['rank_result'].keys():
-                champion = get_object_or_404(Champion, champion_key=rank_result_champion)
-
-                win = result['rank_result'][rank_result_champion]['win']
-                lose = result['rank_result'][rank_result_champion]['lose']
-                games = result['rank_result'][rank_result_champion]['games']
-                kills = result['rank_result'][rank_result_champion]['kills']
-                deaths = result['rank_result'][rank_result_champion]['deaths']
-                assists = result['rank_result'][rank_result_champion]['assists']
-
-                try:
-                    average = round((result['rank_result'][rank_result_champion]['kills'] +
-                                     result['rank_result'][rank_result_champion]['assists']) /
-                                    result['rank_result'][rank_result_champion]['deaths'], 2)
-                except ZeroDivisionError:
-                    average = None
-
-                winning_rate = round(
-                    result['rank_result'][rank_result_champion]['win'] / result['rank_result'][rank_result_champion][
-                        'games'] * 100)
-
-                RankGameResult.objects.create(champion=champion, summoner=summoner,
-                                              win=win,
-                                              lose=lose,
-                                              games=games,
-                                              kills=kills,
-                                              deaths=deaths,
-                                              assists=assists,
-                                              average=average,
-                                              winning_rate=winning_rate
-                                              )
+            # for rank_result_champion in result['rank_result'].keys():
+            #    self.create_rank_result(result, created)
+            #    champion = get_object_or_404(Champion, champion_key=rank_result_champion)
             context['summoner'] = created
             context['rank_result'] = RankGameResult.objects.filter(summoner=created)
 
         else:
             print('exist')
-            RankGameResult.objects.filter(summoner=summoner).delete()
-            for rank_result_champion in result['rank_result'].keys():
-                champion = get_object_or_404(Champion, champion_key=rank_result_champion)
-
-                win = result['rank_result'][rank_result_champion]['win']
-                lose = result['rank_result'][rank_result_champion]['lose']
-                games = result['rank_result'][rank_result_champion]['games']
-                kills = result['rank_result'][rank_result_champion]['kills']
-                deaths = result['rank_result'][rank_result_champion]['deaths']
-                assists = result['rank_result'][rank_result_champion]['assists']
-                try:
-                    average = round((result['rank_result'][rank_result_champion]['kills'] +
-                                     result['rank_result'][rank_result_champion]['assists']) /
-                                    result['rank_result'][rank_result_champion]['deaths'], 2)
-                except ZeroDivisionError:
-                    average = None
-
-                winning_rate = round(
-                    result['rank_result'][rank_result_champion]['win'] / result['rank_result'][rank_result_champion][
-                        'games'] * 100)
-
-                RankGameResult.objects.create(champion=champion, summoner=summoner,
-                                              win=win,
-                                              lose=lose,
-                                              games=games,
-                                              kills=kills,
-                                              deaths=deaths,
-                                              assists=assists,
-                                              average=average,
-                                              winning_rate=winning_rate
-                                              )
+            # RankGameResult.objects.filter(summoner=summoner).delete()
+            # self.create_rank_result(result, summoner)
             summoner.refreshed_date = timezone.now()
             summoner.save()
             context['summoner'] = summoner
