f0c812b1fb3492f4df22028feaac82719bf12ebe
Junphill Hwang <444.gkk@gmail.com>
2019-09-13 01:30:47 +0900

    comment listing

--- data_app/views.py
+++ data_app/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -25,31 +25,143 @@ class CommentWriteView(View):
 
 
 class CommentListView(View):
+    def comment_format(self, request, comments):
+        comment_list = []
+        comment_sample = """
+        <div class="comment" id="comment_{}">
+            <div class="writer">
+                <p style="margin:35px 0px 20px 0px; padding-left:15px;">{}<span class="comment_writer">{}</span><br><span class="written_date">{}</span></p>
+            </div>
+            <div class="comment_detail">
+                <span class="comment_text">{}</span>
+            </div>
+            <div class="comment_set">
+                <p style="margin-top: 0;margin-bottom: 40%;">
+                    {}
+                    <span class="report" onclick="open_report_modal({})"><i class="fas fa-exclamation-triangle" style="color:orange;"></i>&nbsp;신고</span>
+                </p>
+                <p>
+                    {}
+                    {}
+                </p>
+            </div>
+        </div>
+        """
+
+        tier_sample = '<span class="tier_{}">{}&nbsp;</span>'
+        delete_sample = '<span class="delete" onclick="{}({})"><i class="fas fa-trash" style="color:gray"></i>&nbsp;삭제</span>&nbsp;&nbsp;'
+        like_sample = """
+        <span class="like {}" onclick="like_submit('{}', 'like', '{}')"><i class="far fa-thumbs-up"></i> <span class="like_count">{}</span></span>&nbsp;&nbsp;&nbsp;&nbsp;
+        """
+        dislike_sample = """
+            <span class="dislike {}" onclick="like_submit('{}', 'dislike', '{}')"><i class="far fa-thumbs-down"></i> <span class="dislike_count">{}</span></span>&nbsp;&nbsp;&nbsp;&nbsp;
+            """
+
+        if self.request.user.is_superuser:
+            for x in comments:
+                if x.writer:
+                    tier = tier_sample.format('C', 'C')
+                    delete_element = delete_sample.format('direct_delete', x.id)
+                    name = x.writer.profile.nickname
+
+                else:
+                    delete_element = delete_sample.format('open_delete_modal', x.id)
+                    name = x.nickname
+                    tier = ''
+
+                like = Like.objects.filter(liker=self.request.user, comment__id=x.id)
+
+                if like.count() > 0:
+                    if like[0].like and not like[0].dislike:
+                        like_element = like_sample.format('liked', 'cancel', x.id, x.like)
+                        dislike_element = dislike_sample.format('', 'submit', x.id, x.dislike)
+                    else:
+                        like_element = like_sample.format('', 'submit', x.id, x.like)
+                        dislike_element = dislike_sample.format('disliked', 'cancel', x.id, x.dislike)
+                else:
+                    like_element = like_sample.format('', 'submit', x.id, x.like)
+                    dislike_element = dislike_sample.format('', 'submit', x.id, x.dislike)
+
+                comment_list.append(
+                    comment_sample.format(x.id, tier, name, str(x.written_date)[:16], x.comment, delete_element, x.id, like_element, dislike_element))
+
+        elif self.request.user.is_authenticated:
+            for x in comments:
+                if x.writer:
+                    tier = tier_sample.format('C', 'C')
+                    if x.writer == self.request.user:
+                        delete_element = delete_sample.format('direct_delete', x.id)
+                    else:
+                        delete_element = ''
+
+                    name = x.writer.profile.nickname
+
+                else:
+                    delete_element = delete_sample.format('open_delete_modal', x.id)
+                    name = x.nickname
+                    tier = ''
+
+                like = Like.objects.filter(liker=self.request.user, comment__id=x.id)
+
+                if like.count() > 0:
+                    if like[0].like and not like[0].dislike:
+                        like_element = like_sample.format('liked', 'cancel', x.id, x.like)
+                        dislike_element = dislike_sample.format('', 'submit', x.id, x.dislike)
+                    else:
+                        like_element = like_sample.format('', 'submit', x.id, x.like)
+                        dislike_element = dislike_sample.format('disliked', 'cancel', x.id, x.dislike)
+                else:
+                    like_element = like_sample.format('', 'submit', x.id, x.like)
+                    dislike_element = dislike_sample.format('', 'submit', x.id, x.dislike)
+
+                comment_list.append(
+                    comment_sample.format(x.id, tier, name, str(x.written_date)[:16], x.comment, delete_element, x.id, like_element, dislike_element))
+
+        else:
+            for x in comments:
+                if x.writer:
+                    tier = tier_sample.format('C', 'C')
+                    delete_element = ''
+
+                    name = x.writer.profile.nickname
+
+                else:
+                    delete_element = delete_sample.format('open_delete_modal', x.id)
+                    name = x.nickname
+                    tier = ''
+
+                like = Like.objects.filter(ip_addr=request.session._session['ip'], comment__id=x.id)
+
+                if like.count() > 0:
+                    if like[0].like and not like[0].dislike:
+                        like_element = like_sample.format('liked', 'cancel', x.id, x.like)
+                        dislike_element = dislike_sample.format('', 'submit', x.id, x.dislike)
+                    else:
+                        like_element = like_sample.format('', 'submit', x.id, x.like)
+                        dislike_element = dislike_sample.format('disliked', 'cancel', x.id, x.dislike)
+                else:
+                    like_element = like_sample.format('', 'submit', x.id, x.like)
+                    dislike_element = dislike_sample.format('', 'submit', x.id, x.dislike)
+
+                comment_list.append(
+                    comment_sample.format(x.id, tier, name, str(x.written_date)[:16], x.comment, delete_element, x.id, like_element, dislike_element))
+
+        return comment_list
+
     def post(self, request):
         summoner = get_object_or_404(Summoner, id=self.request.POST.get('summoner'))
         order_type = self.request.POST.get('order_type')
-        comment_list = []
-        comment_sample = '<div class="comment" id="comment_{}"><div class="writer"><p>{}</p><p>{}</p></div><div class="comment_detail"><pre>{}</pre></div><div class="comment_set">삭제 신고</div></div>'
 
         if order_type == 'recent':
             comments = Comment.objects.filter(summoner=summoner).order_by('-written_date')
 
-            for x in comments:
-                if x.writer:
-                    comment_list.append(comment_sample.format(x.id, x.writer.profile.nickname, str(x.written_date)[:16], x.comment))
-                else:
-                    comment_list.append(
-                        comment_sample.format(x.id, x.nickname, str(x.written_date)[:16], x.comment))
+        elif order_type == 'like':
+            comments = Comment.objects.filter(summoner=summoner).order_by('-total')
 
-        if order_type == 'like':
-            comments = Comment.objects.filter(summoner=summoner).order_by('total')
+        else:
+            raise Http404
 
-            for x in comments:
-                if x.writer:
-                    comment_list.append(comment_sample.format(x.writer.profile.nickname, str(x.written_date)[:16], x.comment))
-                else:
-                    comment_list.append(
-                        comment_sample.format(x.nickname, str(x.written_date)[:16], x.comment))
+        comment_list = self.comment_format(self.request, comments)
 
         context = {'comment_list': comment_list}
         return HttpResponse(json.dumps(context), content_type="application/json")
