deac46ffed4d3dda4732ccef9e94db11aa95e22f
Junphill Hwang <444.gkk@gmail.com>
2019-09-16 12:21:41 +0900

    allauth template path

--- insect/settings.py
+++ insect/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'insect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
