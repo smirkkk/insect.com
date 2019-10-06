59de42f3cc0cd543ed83af0f9b42b38fb27d3e67
Junphill Hwang <444.gkk@gmail.com>
2019-10-06 23:54:56 +0900

    champion review summoner name bold

--- data_app/templatetags/rank_record.py
+++ data_app/templatetags/rank_record.py
@@ -4,7 +4,7 @@ register = template.Library()
 @register.filter
 def insert_summoner(value, summoner):
     try:
-        value = value.replace('[소환사]', summoner.summoner_name)
+        value = value.replace('[소환사]', '<span class="review_summoner">' + summoner.summoner_name + '</span>')
     except AttributeError:
         pass
 
